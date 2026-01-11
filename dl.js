f8bf7e313aaf4374ad4a468f58ef45dfa54cc12e
aeryli <72891343+aeryli@users.noreply.github.com>
2026-01-10 20:04:43 -0800

    Update download URL to point to GitHub repository

--- dl.js
+++ dl.js
@@ -1,5 +1,6 @@
+let dUrl = "https://github.com/aeryli/cloud-client/raw/refs/heads/main/index.html"
 let downloading = browser.downloads.download({
-  url: downloadUrl,
+  url: dUrl,
   filename: "index.html",
   conflictAction: "uniquify",
 });
