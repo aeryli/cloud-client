fae767dd026c6194cdde79813d41f18b115cc695
aeryli <72891343+aeryli@users.noreply.github.com>
2026-01-10 20:06:30 -0800

    Encapsulate download logic in dl function

--- dl.js
+++ dl.js
@@ -1,6 +1,8 @@
-let dUrl = "https://github.com/aeryli/cloud-client/raw/refs/heads/main/index.html"
-let downloading = browser.downloads.download({
-  url: dUrl,
-  filename: "index.html",
-  conflictAction: "uniquify",
-});
+function dl() {
+  let dUrl = "https://github.com/aeryli/cloud-client/raw/refs/heads/main/index.html"
+  let downloading = browser.downloads.download({
+    url: dUrl,
+    filename: "index.html",
+    conflictAction: "uniquify",
+  });
+}
